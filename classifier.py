5e7603c9703cd2d614b5dab7052fd0dc96e6f7d1
Federico Madden <maddenfederico@gmail.com>
2021-04-05 17:22:32 -0700

    Switch classifier model from BERT to LSTM + BERT embeddings + more
    Add "return home" button to results page

--- classifier.py
+++ classifier.py
@@ -2,36 +2,59 @@ import os
 import torch
 import torch.nn as nn
 from nltk.tokenize import sent_tokenize, word_tokenize
-from transformers import DistilBertTokenizerFast, DistilBertModel
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from transformers import BertTokenizerFast, BertModel
 from flask import Flask
 
 app = Flask(__name__)
 
-class BERTClassifier(nn.Module):
+
+class LSTM(nn.Module):
     """Defines the classifier powering the webapp. Can be swapped for any PyTorch compatible multi-class multi-label
-    classification model """
+        classification model """
 
-    def __init__(self, config=None, dr=0.5, freeze_bert=True):
+    def __init__(self, dimension=400, text_field=None, num_layers=3, dr=0.201060549754906, bidirectional=True, output_size=434):
         super().__init__()
+        self.embedding = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+        print("Model Init")
+        self.embedding.eval()
 
-        self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased', config=config)
-        # Freeze bert layers
-        if freeze_bert:
-            for param in self.bert.parameters():
-                param.requires_grad = False
-
+        self.dimension = dimension
+        self.lstm = nn.LSTM(input_size=768,
+                            hidden_size=dimension,
+                            num_layers=num_layers,
+                            batch_first=True,
+                            bidirectional=bidirectional)
         self.drop = nn.Dropout(p=dr)
-        self.fc = nn.Linear(768, 434)  # TODO Make this not a magic number, classify() should be agnostic to this number
-        app.logger.info("Model Init")
 
-    def forward(self, _input, attention_mask):
+        self.fc = nn.Linear(2 * dimension, output_size)
 
-        output = self.bert(_input, attention_mask, output_hidden_states=False, output_attentions=False)
-        output = output['last_hidden_state'][:, 0]
-        text_fea = self.drop(output)
-        text_fea = nn.ReLU()(text_fea)
+    def encode(self, token_ids_tensor, attention_mask):
+        """Given output from BertTokenizer.convert_tokens_to_ids() as tensor, feed into BERT and then return the sum
+        of last four hidden layers as a tensor of token embeddings shape (batch_size, max_seq_length, 768)"""
+        with torch.no_grad():
+            outputs = self.embedding(token_ids_tensor, attention_mask)
+
+        hidden_states = outputs[2]
+        embeddings = torch.stack(hidden_states[-4:]).sum(0)
+        return embeddings
+
+    def forward(self, text, text_len, attention_mask):
+        # I'd really like to build a more robust pipeline so I can pre-encode everything,
+        # but it would take a while so we'll see if on-the-fly encoding doesn't kill performance too much
+        text_emb = self.encode(text, attention_mask)
+        packed_input = pack_padded_sequence(text_emb, text_len.cpu(), batch_first=True, enforce_sorted=False)
+        packed_output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
+
+        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
+        out_reverse = output[:, 0, self.dimension:]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+
+        text_fea = self.drop(out_reduced)
         text_fea = self.fc(text_fea)
-        text_out = torch.squeeze(text_fea, 1)
+        text_fea = torch.squeeze(text_fea, 1)
+        text_out = torch.sigmoid(text_fea)
 
         return text_out
 
@@ -47,20 +70,21 @@ def classify(text):
     input """
     # TODO Wonder if there's a better way of doing this so the model persists across fucn calls. Will see once I get
     #  Heroku running
+
     sentences = sent_tokenize(text)
     clean_sentences = list(map(clean_text, sentences))
-    word_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    word_tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
     # tokenize
-    _input = word_tokenizer(clean_sentences, padding=True, return_tensors='pt', return_attention_mask=True)
+    _input = word_tokenizer(clean_sentences, padding=True, return_tensors='pt', return_attention_mask=True, return_length=True)
 
     # pass tokenized text thru model
-    model = BERTClassifier(dr=.3, freeze_bert=True)
+    model = LSTM(dr=.3)
     state_dict = torch.load(os.path.join('model', 'model.pt'), map_location=torch.device('cpu'))
     model.load_state_dict(state_dict['model_state_dict'])
 
     model.eval()
     with torch.no_grad():
-        model_output = model(_input['input_ids'], _input['attention_mask'])
+        model_output = model(_input['input_ids'], _input['length'], _input['attention_mask'])
 
     #  We start with: A list of command names, a list of sentences, a matrix with
     #  each row corresponding to a sentence and each column corresponding to a label's probability of being
@@ -72,9 +96,9 @@ def classify(text):
     label_indices = labels[0].tolist()
     probabilities = labels[1].tolist()
 
-    app.logger.debug(f'Label tensor shape: {labels[1].shape}')
+    with open(os.path.join('resources', 'label_names.txt')) as f:
+        command_names = f.read().splitlines()
 
-    command_names = [f'placeholder{i}' for i in range(434)]  # TODO
     output = dict()
     for i, row in enumerate(probabilities):  # TODO vectorize this if possible
         sent = sentences[i]
